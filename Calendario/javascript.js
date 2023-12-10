2f01af3f21088e75b627d15355e2132f9f53b25f
Tormenta88 <tormenta8282@gmail.com>
2023-12-10 23:56:55 +0100

    MesAño

--- Calendario/javascript.js
+++ Calendario/javascript.js
@@ -62,5 +62,7 @@ closePopup.addEventListener('click', () => {
 })
 
 
-console.log('dwadwajdwahjdkawhhwakd'.length)
-
+//TODO Para los meses
+//const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//const d = new Date("2021-03-25");
+//let month = months[d.getMonth()];
